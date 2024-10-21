14dbd5a104679ecbf0bc25784eb6896a6e22f440
gouthamarcot <gauthamarcot@yahoo.com>
2024-10-21 22:30:30 +0530

    [routes] added routes

--- cfc_be/api/cfc_api/routes/gcp_routes.py
+++ cfc_be/api/cfc_api/routes/gcp_routes.py
@@ -4,5 +4,4 @@ from flask_restx import Namespace, Resource
 from cfc_be.api.cfc_api.extensions import logger
 from cfc_be.api.cfc_api.services.aws_service import aws_services_list
 
-api = Namespace('AWS routes', description='AWS pricing and calculation apis')
-
+api = Namespace('gcp routes', description='Cloud service list api related operations')
