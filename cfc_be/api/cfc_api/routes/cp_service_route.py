b4dee04d8ae6370365174dc27584e1cb1f729db5
gouthamarcot <gauthamarcot@yahoo.com>
2024-10-21 22:35:40 +0530

    added all service list routes

--- cfc_be/api/cfc_api/routes/cp_service_route.py
+++ cfc_be/api/cfc_api/routes/cp_service_route.py
@@ -8,10 +8,40 @@ api = Namespace('cp_service', description='Cloud service list api related operat
 
 
 @api.route('/cp_service/aws')
-class CpServiceList(Resource):
+class AWSCpServiceList(Resource):
     @api.doc('aws cp_service_list')
     def get(self):
         logger.info(f"sending aws cp_service_list to {request.remote_addr}")
+        data = aws_services_list()
+        if data:
+            return data, 200
+
+@api.route('/cp_service/gcp')
+class GCPCpServiceList(Resource):
+    @api.doc('gcp cp_service_list')
+    def get(self):
+        logger.info(f"sending gcp cp_service_list to {request.remote_addr}")
+        data = aws_services_list()
+        if data:
+            return data, 200
+
+# route for azure service list
+@api.route('/cp_service/azure')
+class AzureCpServiceList(Resource):
+    @api.doc('azure cp_service_list')
+    def get(self):
+
+        logger.info(f"sending azure cp_service_list to {request.remote_addr}")
+        data = aws_services_list()
+        if data:
+            return data, 200
+
+# route for oracle cloud service list
+@api.route('/cp_service/oci')
+class OCICpServiceList(Resource):
+    @api.doc('oracle cloud infra cp_service_list')
+    def get(self):
+        logger.info(f"sending oci cp_service_list to {request.remote_addr}")
         data = aws_services_list()
         if data:
             return data, 200
