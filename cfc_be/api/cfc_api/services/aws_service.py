205cad9fa72f4db2c0701d990f98ddfb2146171b
gouthamarcot <gauthamarcot@yahoo.com>
2024-11-27 10:39:12 +0530

    [aws_etl] added aws services etl

--- cfc_be/api/cfc_api/services/aws_service.py
+++ cfc_be/api/cfc_api/services/aws_service.py
@@ -4,19 +4,18 @@ from cfc_be.api.cfc_api.extensions import db_service
 
 def aws_services_list():
     try:
-        db = db_service.get_client()['cfc_main']
+        db = db_service.get_client()['cfc_db']
         col = db['aws_services'].find({})
         res_li = []
         for i in col:
             for j in i['awsServices']:
-                res_li.append({'name': j['name'], 'description': j['description'] if 'description' in j else j[
-                                                                                                                 'name'] + "is an aws service",
+                res_li.append({'name': j['name'], 'description': j['description'] if 'description' in j else j['name'] + "is an aws service",
                                'regions': j['regions'] if len(j['regions']) > 0 else ['global'],
                                'searchKeywords': j['searchKeywords'] if len(
                                    ['searchKeywords']) > 0 and 'searchKeywords' in j else j['name']})
         return res_li if len(res_li) > 0 else None
     except Exception as e:
-        logger.error("error occurred due to ", e)
+        print("error occurred due to ", e)
 
 
 def aws_regions_list():
@@ -29,4 +28,4 @@ def aws_regions_list():
             res_li.append(i)
         return res_li if len(res_li) > 0 else None
     except Exception as e:
-        logger.error("error occurred due to ", e)
+        print(e)
--- cfc_etl/aws_pricing/aws_service_list_etl.py
+++ cfc_etl/aws_pricing/aws_service_list_etl.py
@@ -1,34 +1,37 @@
+import json
+
 import boto3
 from pprint import pprint
+
+import pymongo
 
 client = boto3.client('pricing', region_name='us-east-1')
 service_client = boto3.client('servicecatalog')
 
 
-def get_all_aws_services(next_token='', service_list=None):
+def get_all_aws_services(next_token='', service_list=None, page=None):
     if service_list is None:
         service_list = []
     paginator = client.get_paginator('describe_services')
-    if next_token and len(service_list) > 1:
+    if hasattr(page, 'NextToken') and len(service_list) > 1:
         page_iterator = paginator.paginate(MaxResults=100, NextToken=next_token)
     else:
         page_iterator = paginator.paginate(MaxResults=100)
     for page in page_iterator:
         next_token = page['NextToken']
         for service in page['Services']:
-            service_list.append(service['ServiceCode'])
-        get_all_aws_services(next_token, service_list)
+            service_list.append({'service': service['ServiceCode'], 'search_attributes': service['AttributeNames']})
+        get_all_aws_services(next_token, service_list, page)
     print(service_list)
     # Call the 'get_attribute_values' API to get service names
-    # response = client.describe_services(
-    #     FormatVersion='aws_v1',
-    #     MaxResults=100
-    # )
-    # services = response['Services']
-    # for service in services:
-    #     service_list.append(service['ServiceCode'])
-    #
-    # print(service_list)
+    response = client.describe_services(
+        FormatVersion='aws_v1',
+        MaxResults=100
+    )
+    services = response['Services']
+    for service in service_list:
+        service_list.append(service['ServiceCode'])
+    print(service_list)
 
 
 # Get all AWS services with descriptions
@@ -36,8 +39,25 @@ def get_product_description(service_code):
     res = service_client.describe_product(AcceptLanguage='en-US', Name=service_code)
     print(res['ProductViewSummary'])
 
+mongo_uri = f"mongodb://13.201.34.47:27017/?retryWrites=true&loadBalanced=false&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true"
+
+client = pymongo.MongoClient(mongo_uri)
+
+main_db = client["cfc_db"]
+aws_services = main_db["aws_services"]
+def json_to_monogo(json_path):
+    with open(json_path) as f:
+        data = json.load(f)
+        service_list = []
+        for service in data['awsServices']:
+            service_list.append({'service_code': service['name'],
+                                 'description': service['description'] if hasattr(service, 'description') else f"{service['name']} is an AWS service",
+                                 'service_regions': service['regions'] if hasattr(service, 'regions') and len(service['regions']) > 0 else ['global'],
+                                 'search_words': service['searchKeywords'] if hasattr(service, 'searchKeywords') and len(service['searchKeywords']) > 0 else [service['name']]})
+        aws_services.insert_many(service_list)
 
 if __name__ == '__main__':
-    # 'AWSELB'
-    get_product_description('AWSELB')
+    json_to_monogo(
+        '/Users/gouthamarcot/Documents/TeleportPay/codebase/costformationcalculator/CostFormation/cfc_etl/data'
+        '/ser_des.json')
     # get_all_aws_services(next_token='', service_list=[])
