f51c83c7390364940b15b9d3523add56375749fa
gouthamarcot <gauthamarcot@yahoo.com>
2024-12-01 15:54:55 +0530

    [webapp|api] Estimators added

---
+++ cfc_be/api/cfc_api/services/estimator/aws_estimator.py
@@ -0,0 +1,7 @@
+
+def calculate_aws_cost(service, form_data):
+    pass
+
+
+def get_aws_pricing_data(service):
+    pass
---
+++ cfc_be/api/cfc_api/services/estimator/azure_estimator.py
@@ -0,0 +1,6 @@
+
+def calculate_azure_cost(service, form_data):
+    pass
+
+def get_azure_pricing_data(service):
+    pass
